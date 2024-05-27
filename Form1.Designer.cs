07add7e7a0a7a0d2bac19b86ee8133bc22893dd4
sefa serkan sarı <157028962+dikosa2@users.noreply.github.com>
2024-05-27 18:40:34 +0300

    XMLDosyasiDuzenleme

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace XMLFileSelector
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnSelectXML = new System.Windows.Forms.Button();
+            this.btnSaveXML = new System.Windows.Forms.Button();
+            this.xmlFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.rtbDisplayXML = new System.Windows.Forms.RichTextBox();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.BtnConvertToUTF8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectXML
+            // 
+            this.btnSelectXML.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectXML.Name = "btnSelectXML";
+            this.btnSelectXML.Size = new System.Drawing.Size(200, 50);
+            this.btnSelectXML.TabIndex = 0;
+            this.btnSelectXML.Text = "Dosya Seç";
+            this.btnSelectXML.UseVisualStyleBackColor = true;
+            this.btnSelectXML.Click += new System.EventHandler(this.BtnSelectXML_Click);
+            // 
+            // btnSaveXML
+            // 
+            this.btnSaveXML.Location = new System.Drawing.Point(220, 12);
+            this.btnSaveXML.Name = "btnSaveXML";
+            this.btnSaveXML.Size = new System.Drawing.Size(200, 50);
+            this.btnSaveXML.TabIndex = 1;
+            this.btnSaveXML.Text = "Kaydet";
+            this.btnSaveXML.UseVisualStyleBackColor = true;
+            this.btnSaveXML.Click += new System.EventHandler(this.BtnSaveXML_Click);
+            // 
+            // rtbDisplayXML
+            // 
+            this.rtbDisplayXML.Location = new System.Drawing.Point(12, 68);
+            this.rtbDisplayXML.Name = "rtbDisplayXML";
+            this.rtbDisplayXML.Size = new System.Drawing.Size(776, 370);
+            this.rtbDisplayXML.TabIndex = 2;
+            this.rtbDisplayXML.Text = "";
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(426, 12);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(190, 50);
+            this.BtnRefresh.TabIndex = 3;
+            this.BtnRefresh.Text = "Yenile";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // BtnConvertToUTF8
+            // 
+            this.BtnConvertToUTF8.Location = new System.Drawing.Point(622, 12);
+            this.BtnConvertToUTF8.Name = "BtnConvertToUTF8";
+            this.BtnConvertToUTF8.Size = new System.Drawing.Size(166, 50);
+            this.BtnConvertToUTF8.TabIndex = 4;
+            this.BtnConvertToUTF8.Text = "UTF-8 Dönüştürücü ";
+            this.BtnConvertToUTF8.UseVisualStyleBackColor = true;
+            this.BtnConvertToUTF8.Click += new System.EventHandler(this.BtnConvertToUTF8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnConvertToUTF8);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.btnSaveXML);
+            this.Controls.Add(this.btnSelectXML);
+            this.Controls.Add(this.rtbDisplayXML);
+            this.Name = "Form1";
+            this.Text = "XML File Selector";
+            this.ResumeLayout(false);
+            //
+            //
+            //
+
+        }
+
+        private Button btnSelectXML;
+        private Button btnSaveXML;
+        private OpenFileDialog xmlFileDialog;
+        private RichTextBox rtbDisplayXML;
+        private Button BtnRefresh;
+        private Button BtnConvertToUTF8;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using UtfUnknown;
+
+
+
+namespace XMLFileSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectXML_Click(object sender, EventArgs e)
+        {
+            xmlFileDialog.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+            xmlFileDialog.Title = "Select an XML File";
+
+            if (xmlFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = xmlFileDialog.FileName;
+                try
+                {
+                    // Dosyanın orijinal kodlamasını tespit edip içeriği oku
+                    using (FileStream fileStream = File.OpenRead(filePath))
+                    using (StreamReader reader = new StreamReader(fileStream, true))
+                    {
+                        var encoding = reader.CurrentEncoding; // Orijinal kodlamayı al
+                        string content = reader.ReadToEnd(); // Dosya içeriğini oku
+
+                        // UTF-8 olarak içeriği yeniden kodla ve RichTextBox'a yükle
+                        byte[] bytes = encoding.GetBytes(content);
+                        byte[] utf8Bytes = Encoding.Convert(encoding, Encoding.UTF8, bytes);
+                        string utf8String = Encoding.UTF8.GetString(utf8Bytes);
+
+                        // UTF-8 string'i XDocument olarak parse et ve RichTextBox'a yaz
+                        XDocument doc = XDocument.Parse(utf8String);
+                        rtbDisplayXML.Text = doc.ToString();
+                    }
+                }
+                catch (XmlException ex)
+                {
+                    MessageBox.Show("XML parsing error: " + ex.Message, "XML Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+
+        private void DisplaySelectedTags(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            string[] tagsToDisplay = { "PartyIdentification", "PartyName", "Person", "PartyTaxScheme", "Sender" };
+
+            var selectedContent = tagsToDisplay.SelectMany(tag =>
+                doc.Descendants(tag).Select(element => element.ToString()))
+                .Aggregate((current, next) => current + "\n" + next);
+
+            rtbDisplayXML.Text = selectedContent;
+        }
+
+        private bool CheckTCKN(string filePath)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(filePath);
+                var idElement = doc.Descendants("ID").FirstOrDefault(el => (string)el.Attribute("schemeID") == "TCKN");
+                if (idElement != null)
+                {
+                    string tckn = idElement.Value;
+                    if (tckn.Length != 11)
+                    {
+                        MessageBox.Show("TC Numarası 11 haneli değil.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return true; // Yine de içeriği göster
+                    }
+                }
+                return true; // Her şey düzgünse içeriği göster
+            }
+            catch (System.Xml.XmlException ex)
+            {
+                MessageBox.Show("XML dosyası hatalı: " + ex.Message, "XML Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false; // Hata varsa içeriği gösterme
+            }
+        }
+
+        private void BtnSaveXML_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(rtbDisplayXML.Text) && !string.IsNullOrEmpty(xmlFileDialog.FileName))
+            {
+                try
+                {
+                    XDocument doc = XDocument.Parse(rtbDisplayXML.Text);
+                    // StreamWriter'ı UTF-8 kodlaması ile kullan
+                    using (StreamWriter writer = new StreamWriter(xmlFileDialog.FileName, false, Encoding.UTF8))
+                    {
+                        doc.Save(writer);
+                        MessageBox.Show("XML file has been saved successfully in UTF-8 format.", "Save File", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (XmlException ex)
+                {
+                    MessageBox.Show("XML parsing error: " + ex.Message, "XML Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No file is loaded or text box is empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            // RichTextBox'ın içeriğini temizle
+            rtbDisplayXML.Clear();
+
+            // OpenFileDialog'ın seçili dosyasını sıfırla
+            xmlFileDialog.FileName = string.Empty;
+
+            // Kullanıcıya bilgi ver
+            MessageBox.Show("Yeni bir dosya seçebilirsiniz.", "Yenilendi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnConvertToUTF8_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(xmlFileDialog.FileName))
+            {
+                try
+                {
+                    string filePath = xmlFileDialog.FileName;
+                    byte[] fileContentBytes = File.ReadAllBytes(filePath);
+
+                    // Kodlama tespiti yap
+                    var detectionResult = CharsetDetector.DetectFromBytes(fileContentBytes);
+                    Encoding originalEncoding = detectionResult.Detected?.Encoding;
+
+                    string content;
+                    if (originalEncoding != null)
+                    {
+                        // Dosyayı orijinal kodlamayla oku
+                        content = originalEncoding.GetString(fileContentBytes);
+                    }
+                    else
+                    {
+                        // Varsayılan olarak UTF-8 kullan
+                        content = Encoding.UTF8.GetString(fileContentBytes);
+                    }
+
+                    // İçeriği UTF-8'e dönüştür
+                    byte[] utf8Bytes = Encoding.Convert(originalEncoding ?? Encoding.UTF8, Encoding.UTF8, originalEncoding?.GetBytes(content) ?? fileContentBytes);
+                    string utf8Content = Encoding.UTF8.GetString(utf8Bytes);
+
+                    rtbDisplayXML.Text = utf8Content;
+                    MessageBox.Show("Dosya başarıyla UTF-8 formatına dönüştürüldü.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Dönüştürme sırasında bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen önce bir dosya seçin.", "Dosya Seçilmedi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+
+
+    }
+}
